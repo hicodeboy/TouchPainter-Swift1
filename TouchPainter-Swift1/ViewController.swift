39effd0a93bfc7a9bb8c0290846b422844c6262b
hicodeboy <2685312867@qq.com>
2021-05-29 21:24:01 +0800

    初始化

---
+++ TouchPainter-Swift1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TouchPainter-Swift1
+//
+//  Created by hicodeboy on 2021/5/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
